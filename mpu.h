cf09d523acf37db19f928bed001f5175ce35de43
Aditya Setyo Lutfiandhika <sladityaa176@gmail.com>
2026-07-26 06:49:04 +0700

    revisi mpu

--- mpu.h
+++ mpu.h
@@ -1,52 +1,36 @@
 #include <Wire.h>
 #include <MPU6050_light.h>
+#include <ArduinoJson.h>
 
 class Mpu {
   private:
     MPU6050 mpu;
-    unsigned long lastPrint;   
+    unsigned long lastSend;
 
   public:
-    Mpu() : mpu(Wire) {
-      lastPrint = 0;
+    Mpu() : mpu(Wire) { lastSend = 0; }
+
+    void setup() {
+      Wire.begin();
+      byte status = mpu.begin();
+      Serial.print("Status MPU6050: "); Serial.println(status);
+      delay(1000);
+      mpu.calcOffsets();
     }
 
-  void setup() {
-    Wire.begin();
-    byte status = mpu.begin();
-    Serial.print("Status MPU6050: ");
-    Serial.println(status);
-
-    // Kalibrasi sensor
-    Serial.println("Mengkalibrasi MPU...");
-    delay(1000);
-    mpu.calcOffsets();
-    Serial.println("Kalibrasi selesai!");
-  }
-
-  void loop() {
-    mpu.update();
-
-    if (millis() - lastPrint > 1000) { // biar ngga spam
-      float ax = mpu.getAccX();
-      float ay = mpu.getAccY();
-      float az = mpu.getAccZ();
-      float gx = mpu.getGyroX();
-      float gy = mpu.getGyroY();
-      float gz = mpu.getGyroZ();
-
-      Serial.print(F("ACCEL X: ")); Serial.print(ax);
-      Serial.print(F(" Y: "));      Serial.print(ay);
-      Serial.print(F(" Z: "));      Serial.println(az);
-      Serial.print(F("GYRO  X: ")); Serial.print(gx);
-      Serial.print(F(" Y: "));      Serial.print(gy);
-      Serial.print(F(" Z: "));      Serial.println(gz);
-
-      String dataMPU = "MPU," + String(ax) + "," + String(ay) + "," + String(az) + "," +
-                        String(gx) + "," + String(gy) + "," + String(gz);
-      Serial.println(dataMPU);
-
-      lastPrint = millis();
+    void loop() {
+      mpu.update();
+      if (millis() - lastSend > 100) { // rate sesuain kebutuhan PID yaw
+        JsonDocument doc;
+        doc["ax"] = mpu.getAccX();
+        doc["ay"] = mpu.getAccY();
+        doc["az"] = mpu.getAccZ();
+        doc["gx"] = mpu.getGyroX();
+        doc["gy"] = mpu.getGyroY();
+        doc["gz"] = mpu.getGyroZ();
+        serializeJson(doc, Serial);
+        Serial.println();
+        lastSend = millis();
+      }
     }
-  }
 };
